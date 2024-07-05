a125432f0d65053305f10064996c2857c2722ae5
Sturmsee <nschiff98@web.de>
2024-07-05 14:45:03 +0200

    created fundamentals

---
+++ scripts/MainMenu.ts
@@ -0,0 +1,9 @@
+var startButton: HTMLDivElement;
+var ruleButton: HTMLDivElement;
+
+ruleButton = <HTMLDivElement>document.querySelector("rules"); 
+startButton = <HTMLDivElement>document.querySelector("start");
+
+function OnClick(event: any): void {
+    
+}
