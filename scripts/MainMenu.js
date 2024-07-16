f03aecab04f7b472554408ef9fb5b8d68e2ab91d
Sturmsee <nschiff98@web.de>
2024-07-16 14:21:23 +0200

    complied time function

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -4,6 +4,7 @@ var ruleButton;
 window.onload = () => {
     ruleButton = document.querySelector("rules");
     startButton = document.querySelector("start");
+    startButton.addEventListener("click", () => OnClick(event, "game.html"));
 };
 function OnClick(event, page) {
     window.location.href = page;
--- scripts/game.js
+++ scripts/game.js
@@ -3,9 +3,16 @@ var iceshop;
 (function (iceshop) {
     //var orders: Order[] = [];
     var canvas;
+    var time;
+    var cooldown = 500;
     window.onload = () => {
         canvas = document.querySelector('canvas');
+        time = performance.now();
     };
     function customerSpawnTimer() {
+        if (time - performance.now() > cooldown) {
+            time = performance.now();
+            return true;
+        }
     }
 })(iceshop || (iceshop = {}));
